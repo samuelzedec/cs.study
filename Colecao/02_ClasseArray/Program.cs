3dbf0c8f4290f72a8cb8393e22a86f75326d0cb3
Samuel Zedec <samuelzedec@gmail.com>
2024-08-23 20:31:28 -0400

    Exercicio Array

---
+++ Colecao/02_ClasseArray/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("## Classe Array ##\n");
+
+string[] nomes = { "Ana", "Maria", "Dinair", "Paulo", "Carlos", "Beatriz" };
+
+Console.WriteLine("Exibindo Array Original...");
+ExibirArray(nomes);
+
+Console.WriteLine("\n\nInvertendo a ordem do Array...");
+Array.Reverse(nomes);
+ExibirArray(nomes);
+
+Console.WriteLine("\n\nOrdenando o Array...");
+Array.Sort(nomes);
+ExibirArray(nomes);
+
+Console.WriteLine("\n\nLocalizando um item no Array...");
+Console.WriteLine("Informe um nome: ");
+string? nome = Console.ReadLine();
+
+var indice = Array.BinarySearch(nomes, nome);
+
+if(indice >= 0)
+{
+    Console.WriteLine($"O nome foi encotrado no indice {indice}");
+}
+else
+{
+    Console.WriteLine("Nome não encontrado...");
+}
+
+
+Console.ReadKey();
+static void ExibirArray(string[] nomes)
+{
+    foreach (var i in nomes)
+    {
+        Console.Write($"{i} ");
+    }
+}
---
+++ Colecao/03_Exercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("## Exercício ##\n");
+
+string[] nome = new string[5];
+double[] nota = new double[5];
+
+for(int i = 0; i < 5; i++)
+{
+    Console.Write("Informe o nome do aluno: ");
+    nome[i] = Console.ReadLine();
+
+    Console.Write($"Informe a nota de {nome[i]}: ");
+    nota[i] = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine();
+}
+
+foreach(string nomes in nome)
+{
+    Console.WriteLine($"Nome: {nomes}");
+}
+Console.WriteLine();
+
+double media = 0;
+
+foreach(double notas in nota)
+{
+    Console.WriteLine($"Nota: {notas}");
+    media += notas;
+}
+
+Console.WriteLine($"Media: {media / 5}");
